0946dab411a4be77a8ad7210b85560d0cf24ca29
GainetdinovAlbert <104084337+GainetdinovAlbert@users.noreply.github.com>
2022-07-13 14:11:37 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+
+let budget = prompt("Ваш бюджет на месяц?");
+let title = prompt("Название вашего магазина?");
+prompt("Какой тип товаров будем продавать?", "носки");
+prompt("Какой тип товаров будем продавать?", "носки");
+prompt("Какой тип товаров будем продавать?", "носки");
+let shopGoods = ["носки", "носки", "носки"];
+let employers;
+let num = 33721;
+
+
+mainList = {
+    budget,
+    title,
+    shopGoods,
+    open: false
+};  
+
+
+alert("бюджет на 1 день: 1000 рублей");
+console.log(3*3*7*2*1);
+
+
+num **= 3;
+alert(String(num).slice(0,2));
+
+
+
+
+
+
+
